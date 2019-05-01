49070aaef21858f0a30031b4006092964c2a24ca
ls4cfk <ls4cfk@gmail.com>
2019-05-01 21:20:15 +0400

    Beautied code

--- face_detection.py
+++ face_detection.py
@@ -16,20 +16,20 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h) ,(255 ,0 ,0) ,2)
+        roi_gray = gray[y: y + h, x: x + w]
+        roi_color = frame[y: y + h, x: x + w]
 
         eyes = eye_cascade.detectMultiScale(roi_gray)
         success, image = cap.read()
-        crop_img = image[y:y+h, x:x+w]
+        crop_img = image[y: y + h, x: x + w]
 
         cv2.imwrite(f"./faces/frame{number_of_frame}.jpg", crop_img)
         number_of_frame += 1
 
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0,255,0), 2)
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
