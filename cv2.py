58236109d71b4c85bf7b1bd4fd2ed5e7a757c3b2
jacobs0718 <46194511+jacobs0718@users.noreply.github.com>
2019-05-03 23:58:51 +0400

    Add files via upload
    facedetection

--- cv2.py
+++ cv2.py
@@ -1,9 +1,19 @@
-import cv2
 import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
-img = cv2.imread('bookpage.jpg')
-retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
-cv2.imshow('original',img)
-cv2.imshow('threshold',threshold)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+img = cv2.imread('opencv-python-foreground-extraction-tutorial.jpg')
+mask = np.zeros(img.shape[:2],np.uint8)
+
+bgdModel = np.zeros((1,65),np.float64)
+fgdModel = np.zeros((1,65),np.float64)
+
+rect = (161,79,150,150)
+
+cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+img = img*mask2[:,:,np.newaxis]
+
+plt.imshow(img)
+plt.colorbar()
+plt.show()
